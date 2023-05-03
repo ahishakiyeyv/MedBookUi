716bae3063570f9bd57a1f58ea57ff2886426c17
ahishakiyeyv <yvanahishakiye6@gmail.com>
2023-05-03 15:43:52 +0200

    design changed

--- src/views/admin/AppointmentDetails.vue
+++ src/views/admin/AppointmentDetails.vue
@@ -5,20 +5,21 @@
             <h3>Details Rendez-Vous</h3>
         </div>
         <Modal v-bind:actualiser="actualiser" v-bind:toggleModale="toggleModale" v-if="actualiser"></Modal>
-        <div class="modale">
-            <router-link to="/AdminAppointment" class="btn-ba"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></router-link>
+      <div class="action">
+             <router-link to="/AdminAppointment" class="btn-ba"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></router-link>
             <button class="btn-back" v-on:click="toggleModale">Actualiser Status</button>
-            
+      </div>
+      <div class="modale">
             <div class="body-container" v-for="appointment in appointments" :key="appointment.id">
                 <div class="body1">
                     <h2>Numero: <span>{{ appointment.numero_ordre}}-0{{appointment.id}}</span></h2>
                     <h2>Nom: <span>{{ appointment.nom}}</span></h2>
                     <h2>Prenom: <span>{{appointment.prenom}}</span></h2>
                     <h2>Age: <span>{{appointment.age}}</span></h2>
-                    <h2>Sexe: <span>{{appointment.sexe}}</span></h2>
+                    
                 </div>
                 <div class="body2">
-                    
+                    <h2>Sexe: <span>{{appointment.sexe}}</span></h2>
                     <h2>Date: <span>{{appointment.date_arrive}}</span></h2>
                     <h2>Service: <span>{{appointment.service}}</span></h2>
                     <h2>Status:
@@ -28,14 +29,14 @@
                          <span v-if="appointment.status ===null" class="inconnu">Pas Encore actualiser!</span>
                          
                          </h2>
-                    <h2>Message: <span>{{appointment.remarque}}</span></h2>
-                    <h2></h2>
+                    
           
                     <!-- <pre>
                         {{ JSON.stringify(appointment,null,2) }}
                     </pre> -->
                         
                     </div>
+                    <div class="message">Message: <span>{{appointment.remarque}}</span></div>
                 </div>
                 <!-- <h2>Status: 
                             <select name="" v-model="form.status">
@@ -128,7 +129,7 @@ export default {
     margin-top:1.5rem;
     margin-left:2rem;
     border-radius:5px;
-    background:#731acc;
+    background:#416991;
     width:1020px;
     padding:1rem 1rem;
 }
@@ -137,13 +138,20 @@ export default {
     font-weight:normal;
 }
 .btn-back{
-    background: #731acc;
+    background: #416991;
     border-radius:5px;
     padding:0.5rem 1rem;
-    float:right;
     border:none;
     color:#fff;
     font-size:0.8rem;
+
+}
+.action{
+    width:1020px;
+    margin-left:2rem;
+    margin-top:1.5rem;
+    display: flex;
+    justify-content: space-between;
 }
 
 .modale{
@@ -152,16 +160,23 @@ export default {
 }
 
 .body-container{
-    display:flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top:30px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-column-gap: 120px;
+    grid-row-gap: 50px;
+    
 }
-
+.body1{
+    width:400px;
+}
+.body2{
+    width:450px;
+}
 .body1 h2{
     margin-top:40px;
     font-size:1.4rem;
-    color:#333;
+    color:#416991;
 }
 h2 span{
     font-weight:400;
@@ -170,44 +185,45 @@ h2 span{
     margin-left:30px;
     font-size:1.3rem;
 }
-h2 select{
+/* h2 select{
     width:300px;
     height: 30px;
     border:none;
-    border-bottom:1px solid #731acc;
+    border-bottom:1px solid #416991;
     font-size:1rem;
     padding:0 0.5rem;
     background: transparent;
-    color:#731acc;
+    color:#416991;
     margin-left:30px;
     outline: none;
-}
+} */
 
 .body2 h2{
      margin-top:40px;
     font-size:1.4rem;
-    color:#333;
+    color:#416991;
 }
-.h2{
-    margin-left:40px;
-    margin-top:40px;
-    color:#731acc;
-}
-.para{
-    border:1px solid red;
-    margin:5px 40px;
-    padding:10px;
-    color:#7e7c7c;
-}
+
 .btn-ba{
-    float:left;
-    border:1px solid #731acc;
-    padding:0.2rem 1rem;
+    padding:0.1rem 0.6rem;
     border-radius:5px;
+    background:#416991;
 }
 .btn-ba i{
-    font-size:2rem;
-    color:#731acc;
+    font-size:1.6rem;
+    color:#fff;
+}
+.message{
+    font-size:1.4rem;
+    font-weight:bold;
+    width:400px;
+    height:auto;
+    color:red;
+}
+.message span{
+    font-weight:normal;
+    font-size:1.2rem;
+    color:#333;
 }
 /* .text{
     margin-top:10px;
@@ -221,15 +237,7 @@ h2 select{
     font-size:1rem;
     color:#731acc;
 } */
-/* .message{
-    height:50px;
-    width:300px;
-    outline:none;
-    border:1px solid #731acc;
-    border-radius:5px;
-    padding:5px;
-    color:#731acc;
-}
+/* 
 .btn-send{
     width:200px;
     height:40px;
