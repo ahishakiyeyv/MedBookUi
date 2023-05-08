b6e13d388430d1348fa02cdb6e7948b415b916c4
ahishakiyeyv <yvanahishakiye6@gmail.com>
2023-05-08 11:04:25 +0200

    user design added

---
+++ src/views/admin/Utilisateur.vue
@@ -0,0 +1,17 @@
+<template>
+    <Dashboard></Dashboard>
+</template>
+<script>
+import axios from 'axios'
+import Dashboard from '@/components/Dashboard.vue'
+export default {
+    name:'Utilisateur',
+    components:{
+        Dashboard
+    }
+   
+}
+</script>
+<style scoped>
+
+</style>
